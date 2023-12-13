4efdeacdb7367abaf30a2b01b4fa9d2bfed93a30
paulina <paulinalopezholguin1@gmail.com>
2023-12-13 14:41:10 +0100

    VERTICAL- physics, dynamics of stars, sky background and gamelogic - DONT MERGE

--- MoonSlide/MoonSlide/GameScene.swift
+++ MoonSlide/MoonSlide/GameScene.swift
@@ -8,21 +8,42 @@
 import SpriteKit
 import GameplayKit
 
+struct PhysicsCategory{
+    static let none : UInt32 = 0
+    static let all : UInt32 = UInt32.max
+    static let moon : UInt32 = 0b1
+    static let star : UInt32 = 0b10
+}
+
 class GameScene: SKScene {
+    var background = SKSpriteNode(imageNamed: "nightSky")
     var ground = SKSpriteNode()
     var star = SKSpriteNode()
     var cloud = SKSpriteNode()
     var moon = SKSpriteNode()
     private var currentNode: SKNode?
-
+    var gameLogic: GameLogic = GameLogic.shared
+    // Keeps track of when the last update happend.
+    // Used to calculate how much time has passed between updates.
+    var lastUpdate: TimeInterval = 0
     
     override func didMove(to view: SKView){
-        self.anchorPoint = CGPoint(x: 0.5, y:0.5)
         
-        createGrounds()
-        createStars()
-        createClouds()
-        createMoon()
+        let background = SKSpriteNode(imageNamed: "nightSky")
+                
+        background.size = size
+        
+        background.position = CGPoint(x: frame.midX, y: frame.midY)
+        
+        background.zPosition = -1
+        
+        addChild(background)
+        
+        
+        //self.anchorPoint = CGPoint(x: 0.5, y:0.5)
+        self.setUpGame()
+        self.setUpPhysicsWorld()
+        
         
     }
     
@@ -63,7 +84,7 @@ class GameScene: SKScene {
     func createGrounds(){
         for i in 0...2{
             
-            let ground = SKSpriteNode(imageNamed: "ground")
+            let ground = SKSpriteNode(imageNamed: "cloudGround")
             ground.name = "Ground"
             ground.size = CGSize(width: (self.scene?.size.width)!, height: 250)
             ground.anchorPoint = CGPoint(x: 0.5, y:0.5)
@@ -159,8 +180,139 @@ class GameScene: SKScene {
         self.addChild(moon)
         
     }
+}
+
+// MARK: - Game Scene Set Up
+extension GameScene {
     
+    private func setUpGame() {
+        self.gameLogic.setUpGame()
+        
+        /*createGrounds()
+        createStars()
+        createClouds()
+        createMoon()*/
+        
+        let moonInitialPosition = CGPoint(x: 0.5, y: 0.5)
+         self.createPlayer(at: moonInitialPosition)
+         self.startStarsCycle()
+    }
     
+    private func setUpPhysicsWorld() {
+        // TODO: Customize!
+        //physicsWorld.gravity = CGVector(dx: 0, dy: -0.9)
+        physicsWorld.contactDelegate = self
+    }
     
+    private func restartGame() {
+        self.gameLogic.restartGame()
+    }
     
+    private func createPlayer(at position: CGPoint) {
+        
+        self.moon = SKSpriteNode(imageNamed: "moon")
+        self.moon.name = "Moon"
+        self.moon.size = CGSize(width: 200, height: 200)
+        self.moon.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        
+        self.moon.position = position
+        
+        moon.physicsBody = SKPhysicsBody(circleOfRadius: 100.0)
+        moon.physicsBody?.affectedByGravity = false
+        
+        moon.physicsBody?.categoryBitMask = PhysicsCategory.moon
+        
+        moon.physicsBody?.contactTestBitMask = PhysicsCategory.star
+        moon.physicsBody?.collisionBitMask = PhysicsCategory.star
+        
+        let xRange = SKRange(lowerLimit: 0, upperLimit: frame.width - 15)
+        
+        let xConstraint = SKConstraint.positionX(xRange)
+        
+        self.moon.constraints = [xConstraint]
+        addChild(self.moon)
+    }
+    
+    func startStarsCycle() {
+        let createStarAction = SKAction.run(createStar)
+        let waitAction = SKAction.wait(forDuration: 1.0)
+        
+        let createAndWaitAction = SKAction.sequence([createStarAction, waitAction])
+        let starCycleAction = SKAction.repeatForever(createAndWaitAction)
+        
+        run(starCycleAction)
+    }
+}
+
+// MARK: - Stars
+extension GameScene {
+    
+    private func createStar() {
+        let starPosition = self.randomStarPosition()
+        newStar(at: starPosition)
+    }
+    
+    private func randomStarPosition() -> CGPoint {
+        /*let initialX: CGFloat = 25
+         let finalX: CGFloat = self.frame.width - 25
+         
+         let positionX = CGFloat.random(in: initialX...finalX)
+         let positionY = frame.height - 25
+         
+         return CGPoint(x: positionX, y: positionY)*/
+        
+        let initialY: CGFloat = 0.5
+        let finalY: CGFloat = self.frame.height
+        
+        let positionY = CGFloat.random(in: initialY...finalY)
+        let positionX = frame.width + 5
+        
+        return CGPoint(x: positionX, y: positionY)
+    }
+    
+    private func newStar(at position: CGPoint) {
+        
+        let newStar = SKSpriteNode(imageNamed: "star")
+        newStar.name = "Star"
+        newStar.size = CGSize(width: 30, height: 30)
+        newStar.position = CGPoint(x: CGFloat() * star.size.width, y: -(self.frame.size.height / 2))
+        newStar.physicsBody = SKPhysicsBody (circleOfRadius: 25.0)
+        newStar.physicsBody?.affectedByGravity = true
+        
+        newStar.position = position
+        
+        newStar.physicsBody?.categoryBitMask = PhysicsCategory.star
+        newStar.physicsBody?.collisionBitMask = PhysicsCategory.moon
+        
+        addChild(newStar)
+        
+        moveStars()
+        
+        newStar.run(SKAction.sequence([
+            SKAction.wait(forDuration: 5.0),
+            SKAction.removeFromParent()
+        ]))
+    }
+    
+}
+
+
+
+//MARK: - Contacts and Collisions
+extension GameScene : SKPhysicsContactDelegate{
+    
+    func didBegin(_ contact: SKPhysicsContact){
+        let firstBody: SKPhysicsBody = contact.bodyA
+        let secondBody: SKPhysicsBody = contact.bodyB
+        
+        if let node  = firstBody.node, node.name == "Star" {
+            node.removeFromParent()
+        }
+        
+        if let node = secondBody.node, node.name == "Star" {
+            node.removeFromParent()
+        }
+        
+        print("Contact happened!")
+    }
 }
